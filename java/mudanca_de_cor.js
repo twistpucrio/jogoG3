cefb924d9985a68837da2494b152ed3153916493
rafaela200601 <rafinhaestudos.2006@gmail.com>
2025-09-01 17:57:18 -0300

    aaaaaa

--- java/mudanca_de_cor.js
+++ java/mudanca_de_cor.js
@@ -4,11 +4,14 @@ document.addEventListener("DOMContentLoaded", () => {
   const cssPrincipal = document.getElementById("css_principal");
   const cssBox = document.getElementById("css_box");
   const cssFormat = document.getElementById("css_format");
+  const cssModal=document.getElementById("css_modal");
 
   if (localStorage.getItem("acessibilidade") === "on") {
     cssPrincipal.setAttribute("href", "styles/acessibilidade.css");
     cssBox.setAttribute("href", "styles/box_acessibilidade.css");
-    cssFormat.setAttribute("href", "styles/format_acessibilidade.css")
+    cssFormat.setAttribute("href", "styles/format_acessibilidade.css");
+    cssModal.setAttribute("href", "styles/modalAcess.css");
+
   }
 
   btn.addEventListener("click", () => {
@@ -16,11 +19,13 @@ document.addEventListener("DOMContentLoaded", () => {
       cssPrincipal.setAttribute("href", "styles/acessibilidade.css");
       cssBox.setAttribute("href", "styles/box_acessibilidade.css");
       cssFormat.setAttribute("href", "styles/format_acessibilidade.css");
+      cssModal.setAttribute("href", "styles/modalAcess.css");
       localStorage.setItem("acessibilidade", "on");
     } else {
       cssPrincipal.setAttribute("href", "styles/estilo.css");
       cssBox.setAttribute("href", "styles/box.css");
       cssFormat.setAttribute("href", "styles/format.css");
+       cssModal.setAttribute("href", "styles/tentarmod.css");
       localStorage.setItem("acessibilidade", "off");
     }
   });
